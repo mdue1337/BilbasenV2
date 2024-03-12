81a824bb21349fd54a579bf92720fd8d8a8c14de
mdue1337 <martin@familiendue.dk>
2024-03-12 18:18:37 +0100

    .

--- MVCtest3d/Controllers/UserController.cs
+++ MVCtest3d/Controllers/UserController.cs
@@ -92,16 +92,20 @@ namespace MVCtest3d.Controllers
         [HttpPost]
         public IActionResult PasswordUpdate(int Id, string Password, string confirmPSW)
         {
-            if (Password != confirmPSW)
+            if (Password is null || confirmPSW is null)
+            {
+                TempData["ErrorMessage"] = "Please enter a password";
+                return RedirectToAction("PasswordUpdate", "User", new { id = Id });
+            }
+            else if (Password != confirmPSW)
             {
                 TempData["ErrorMessage"] = "Passwords do not match.";
-                return RedirectToAction("PasswordUpdate", "User", Id);
+                return RedirectToAction("PasswordUpdate", "User", new { id = Id });
             }
-
-            string encrypted = EncryptionHelper.ComputeSha256Hash(Password);
 
             try
             {
+                string encrypted = EncryptionHelper.ComputeSha256Hash(Password);
                 _db.UpdatePassword(encrypted, Id);
                 _db.ActivateAccount(Id);
                 return RedirectToAction("Information", "User", new { id = Id });
@@ -109,7 +113,7 @@ namespace MVCtest3d.Controllers
             catch (Exception)
             {
                 TempData["ErrorMessage"] = "Failed updating password, please try again";
-                return RedirectToAction("PasswordUpdate", "User", Id);
+                return RedirectToAction("PasswordUpdate", "User", new { id = Id });
             }
         }
     }
